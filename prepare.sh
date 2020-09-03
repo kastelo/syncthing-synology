b36895a3c3c6c0c34c7f09bbf063c0885c3042cd
Jakob Borg <jakob@kastelo.net>
2020-09-03 08:35:15 +0200

    Allow overriding Go version used for build

--- prepare.sh
+++ prepare.sh
@@ -8,6 +8,10 @@ if [[ -z ${syncthing_version:-} ]] ; then
         | tr -d \",v)
 fi
 
+if [[ -z ${go_version:-} ]] ; then
+	go_version=latest
+fi
+
 ver="${syncthing_version}"
 if [[ ! -z ${extra_version:-} ]] ; then
     ver="$syncthing_version.$extra_version"
@@ -39,6 +43,7 @@ build() {
     goos=$1
     goarch=$2
     goarm=${3:-}
+    echo Build "$goos-$goarch $goarm" using "golang:$go_version"
     docker run --rm \
         -v $(pwd):/syncthing \
         -v /tmp/syncthing-gopath:/go \
@@ -46,9 +51,9 @@ build() {
         -w /syncthing \
         -e BUILD_HOST=kastelo.net \
         -e BUILD_USER=synology \
-        golang:latest \
+        "golang:$go_version" \
         go run build.go -no-upgrade -goos $goos -goarch $goarch build
-        mv syncthing "$dst/bin/syncthing-$goos-$goarch$goarm"
+    mv syncthing "$dst/bin/syncthing-$goos-$goarch$goarm"
 }
 
 build linux amd64
